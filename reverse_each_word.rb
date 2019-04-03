1fb409681a841af26283652621d54bd48b13a6ed
Henry Koehler <henryk1229@gmail.com>
2019-04-03 21:29:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
+  reverse_array = []
   array.each do |word|
-    puts word.reverse 
+    reverse_array.push(word.reverse) 
   end
 end
 
